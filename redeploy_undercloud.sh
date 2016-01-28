26c1aa21caf31375664bdc6530f89642c9ba73b0
David Hill <dhill@redhat.com>
2016-01-27 20:35:00 -0500

    Creating some functions in redeploy_undercloud.sh

--- redeploy_undercloud.sh
+++ redeploy_undercloud.sh
@@ -1,22 +1,69 @@
-heat=$( heat stack-list | grep overcloud )
-if [ ! -z "$heat" ]; then
-  heat stack-delete overcloud
-  while [ ! -z "$heat" ]; do
-    heat=$( heat stack-list | grep overcloud )
-    echo -n "."
-    if [[ "$heat" =~ FAILED ]]; then
-      echo "Stack deletion failed... retrying!"
-      heat stack-delete overcloud
+cleanup {
+  rm overcloudrc
+  yum remove -y openstack-* python-oslo-*
+  yum remove -y mariadb
+  rm -rf /var/lib/mysql
+  yum install -y python-rdomanager-oscplugin
+}
+
+delete_old_stack {
+  heat=$( heat stack-list | grep overcloud )
+  if [ ! -z "$heat" ]; then
+    heat stack-delete overcloud
+    while [ ! -z "$heat" ]; do
+      heat=$( heat stack-list | grep overcloud )
+      echo -n "."
+      if [[ "$heat" =~ FAILED ]]; then
+        echo "Stack deletion failed... retrying!"
+        heat stack-delete overcloud
+      fi
+    done
+  fi
+}
+
+create_flavors {
+    for profile in control compute ceph-storage; do
+      openstack flavor create --id auto --ram 6144 --disk 40 --vcpus 4 $profile
+      openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="$profile" $profile
+    done
+
+    openstack flavor create --id auto --ram 4096 --disk 40 --vcpus 4 baremetal
+}
+
+tag_hosts {
+    inc=0
+    for p in $(ironic node-list | grep available | awk '{ print $2 }'); do
+      if [ $inc -lt 3 ]; then
+        ironic node-update $p add properties/capabilities='profile:control,boot_option:local'
+      elif [ $inc -lt 6 ]; then
+        ironic node-update $p add properties/capabilities='profile:cep-storage,boot_option:local'
+      else
+        ironic node-update $p add properties/capabilities='profile:compute,boot_option:local'
+      fi
+      inc=$( expr $inc + 1)
+    done
+}
+
+deploy_overcloud {
+  if [ -d  /home/stack/images ]; then
+    openstack overcloud image upload --image-path /home/stack/images
+    openstack baremetal import --json instackenv.json
+    openstack baremetal configure boot
+    openstack baremetal introspection bulk start
+    create_flavors()
+    tag_hosts()
+    bash create.sh
+    if [ -e overcloudrc ]; then
+      bash setup_images.sh
+      bash create_network.sh
+      bash boot_vm.sh
     fi
-  done
-fi
-
-rm overcloudrc
-
-yum remove -y openstack-* python-oslo-*
-yum remove -y mariadb
-rm -rf /var/lib/mysql
-yum install -y python-rdomanager-oscplugin
+  else 
+    echo "Please download the overcloud-* images and put them in /home/stack/images"
+  fi
+}
+delete_old_stack()
+cleanup()
 
 source stackrc
 openstack undercloud install
@@ -24,38 +71,4 @@ openstack undercloud install
 git clone https://github.com/rthallisey/clapper
 python clapper/network-environment-validator.py
 
-if [ -d  /home/stack/images ]; then
-	openstack overcloud image upload --image-path /home/stack/images
-	openstack baremetal import --json instackenv.json
-	openstack baremetal configure boot
-	openstack baremetal introspection bulk start
-
-	for profile in control compute ceph-storage; do
-	  openstack flavor create --id auto --ram 6144 --disk 40 --vcpus 4 $profile
-	  openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="$profile" $profile
-	done
-
-	openstack flavor create --id auto --ram 4096 --disk 40 --vcpus 4 baremetal
-
-	inc=0
-	for p in $(ironic node-list | grep available | awk '{ print $2 }'); do
-	  if [ $inc -lt 3 ]; then
-	    ironic node-update $p add properties/capabilities='profile:control,boot_option:local'
-	  elif [ $inc -lt 6 ]; then
-	    ironic node-update $p add properties/capabilities='profile:cep-storage,boot_option:local'
-	  else
-	    ironic node-update $p add properties/capabilities='profile:compute,boot_option:local'
-	  fi
-	  inc=$( expr $inc + 1)
-	done
-
-	bash create.sh
-
-	if [ -e overcloudrc ]; then
-		bash setup_images.sh
-		bash create_network.sh
-		bash boot_vm.sh
-	fi
-else 
-  echo "Please download the overcloud-* images and put them in /home/stack/images"
-fi
+deploy_overcloud()
