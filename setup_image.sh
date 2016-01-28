28ee949fb16e96932cd2d2f6be70eb7ce970fdfb
David Hill <dhill@redhat.com>
2016-01-28 16:41:36 -0500

    Adding error handling in setup_images.sh

--- setup_image.sh
+++ setup_image.sh
@@ -7,7 +7,15 @@ fi
 if [ ! -e images/cirros-0.3.4-x86_64-disk.img ]; then
   cd images
   wget http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img
+  rc=$?
   cd ..
 fi
 
-glance image-create --name "cirros-0.3.4-x86_64" --file images/cirros-0.3.4-x86_64-disk.img --disk-format qcow2 --container-format bare --is-public True --progress
+if [ $rc -eq 0 ]; then
+  glance image-create --name "cirros-0.3.4-x86_64" --file images/cirros-0.3.4-x86_64-disk.img --disk-format qcow2 --container-format bare --is-public True --progress
+  rc=$?
+else
+  echo "Wget failed!"
+fi
+
+exit $rc
