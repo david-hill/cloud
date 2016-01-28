7d897b01c0f43715b93c418003e389e6ab094357
David Hill <dhill@redhat.com>
2016-01-27 19:11:34 -0500

    Adding introspection and flavor creation.

--- redeploy_undercloud.sh
+++ redeploy_undercloud.sh
@@ -11,4 +11,11 @@ python clapper/network-environment-validator.py
 
 openstack baremetal import --json instackenv.json
 openstack baremetal configure boot
+openstack baremetal introspection bulk start
 
+for profile in control compute ceph-storage; do
+  openstack flavor create --id auto --ram 6144 --disk 40 --vcpus 4 $profile
+  openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="$profile" $profile
+done
+
+openstack flavor create --id auto --ram 4096 --disk 40 --vcpus 4 baremetal
