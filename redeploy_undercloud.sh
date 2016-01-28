a353e29b6ed38a7062f10c287e9d5a48f18f515f
David Hill <dhill@redhat.com>
2016-01-27 19:44:52 -0500

    Uploading overcloud images and tagging servers automatically.

--- redeploy_undercloud.sh
+++ redeploy_undercloud.sh
@@ -24,6 +24,7 @@ openstack undercloud install
 git clone https://github.com/rthallisey/clapper
 python clapper/network-environment-validator.py
 
+openstack overcloud image upload --image-path /home/stack/images
 openstack baremetal import --json instackenv.json
 openstack baremetal configure boot
 openstack baremetal introspection bulk start
@@ -34,3 +35,17 @@ for profile in control compute ceph-storage; do
 done
 
 openstack flavor create --id auto --ram 4096 --disk 40 --vcpus 4 baremetal
+
+inc=0
+
+for p in $(ironic node-list | grep available | awk '{ print $2 }'); do
+  if [ $inc -lt 3 ]; then
+    ironic node-update $p add properties/capabilities='profile:control,boot_option:local'
+  elif [ $inc -lt 6 ]; then
+    ironic node-update $p add properties/capabilities='profile:cep-storage,boot_option:local'
+  else
+    ironic node-update $p add properties/capabilities='profile:compute,boot_option:local'
+  fi
+  inc=$( expr $inc + 1)
+done
+
