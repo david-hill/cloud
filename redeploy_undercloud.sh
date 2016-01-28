5b30b42344b24c867d0ad8f892eaf1837f6cef25
David Hill <dhill@redhat.com>
2016-01-27 19:01:19 -0500

    Adding openstack undercloud configuration for overcloud

--- redeploy_undercloud.sh
+++ redeploy_undercloud.sh
@@ -1,14 +1,14 @@
 yum remove -y openstack-* python-oslo-*
 yum remove -y mariadb
-
 rm -rf /var/lib/mysql
-
-source stackrc
-
 yum install -y python-rdomanager-oscplugin
 
+source stackrc
 openstack undercloud install
 
 git clone https://github.com/rthallisey/clapper
-
 python clapper/network-environment-validator.py
+
+openstack baremetal import --json instackenv.json
+openstack baremetal configure boot
+
