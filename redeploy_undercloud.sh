4b84d906aabbfac5dea3585f08aee414d125c025
David Hill <dhill@redhat.com>
2016-01-27 21:00:01 -0500

    Some indentation in redeploy_undercloud.sh

--- redeploy_undercloud.sh
+++ redeploy_undercloud.sh
@@ -22,47 +22,47 @@ function delete_overcloud {
 }
 
 function create_flavors {
-    for profile in control compute ceph-storage; do
-      openstack flavor create --id auto --ram 6144 --disk 40 --vcpus 4 $profile
-      openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="$profile" $profile
-    done
+  for profile in control compute ceph-storage; do
+    openstack flavor create --id auto --ram 6144 --disk 40 --vcpus 4 $profile
+    openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="$profile" $profile
+  done
 
-    openstack flavor create --id auto --ram 4096 --disk 40 --vcpus 4 baremetal
+  openstack flavor create --id auto --ram 4096 --disk 40 --vcpus 4 baremetal
 }
 
 function tag_hosts {
-    inc=0
-    for p in $(ironic node-list | grep available | awk '{ print $2 }'); do
-      if [ $inc -lt 3 ]; then
-        ironic node-update $p add properties/capabilities='profile:control,boot_option:local'
-      elif [ $inc -lt 6 ]; then
-        ironic node-update $p add properties/capabilities='profile:cep-storage,boot_option:local'
-      else
-        ironic node-update $p add properties/capabilities='profile:compute,boot_option:local'
-      fi
-      inc=$( expr $inc + 1)
-    done
+  inc=0
+  for p in $(ironic node-list | grep available | awk '{ print $2 }'); do
+    if [ $inc -lt 3 ]; then
+      ironic node-update $p add properties/capabilities='profile:control,boot_option:local'
+    elif [ $inc -lt 6 ]; then
+      ironic node-update $p add properties/capabilities='profile:cep-storage,boot_option:local'
+    else
+      ironic node-update $p add properties/capabilities='profile:compute,boot_option:local'
+    fi
+    inc=$( expr $inc + 1)
+  done
 }
 
 function create_oc_images {
-    openstack overcloud image upload --image-path /home/stack/images
+  openstack overcloud image upload --image-path /home/stack/images
 }
 
 function baremetal_setup {
-    openstack baremetal import --json /home/stack/instackenv.json
-    openstack baremetal configure boot
-    openstack baremetal introspection bulk start
+  openstack baremetal import --json /home/stack/instackenv.json
+  openstack baremetal configure boot
+  openstack baremetal introspection bulk start
 }
 
 function test_overcloud {
-    if [ -e ~/overcloudrc ]; then
-      source ~/overcloudrc
-      bash setup_images.sh
-      bash create_network.sh
-      bash boot_vm.sh
-    else
-      echo "Something weird happened"
-    fi
+  if [ -e ~/overcloudrc ]; then
+    source ~/overcloudrc
+    bash setup_images.sh
+    bash create_network.sh
+    bash boot_vm.sh
+  else
+    echo "Something weird happened"
+  fi
 }
 
 function deploy_overcloud {
