84c57ae067ecfd7f0ed0eb3c249f79ab9768f7d6
David Hill <dhill@redhat.com>
2016-01-28 16:45:17 -0500

    Adding some error handling in redeploy_overcloud.sh

--- redeploy_overcloud.sh
+++ redeploy_overcloud.sh
@@ -10,8 +10,13 @@ function create_overcloud {
     openstack overcloud deploy --templates ~/templates/my-overcloud -e ~/templates/my-overcloud/environments/network-isolation.yaml -e ~/templates/network-environment.yaml -e ~/templates/storage-environment.yaml --control-scale $controlscale --compute-scale $computescale --ceph-storage-scale $cephscale --control-flavor control --compute-flavor compute --ceph-storage-flavor ceph-storage --ntp-server pool.ntp.org --neutron-network-type vxlan --neutron-tunnel-types vxlan
     rc=$?
   fi
+  return $rc
 }
 delete_overcloud
 create_overcloud
-test_overcloud
+rc=$?
+if [ $? -eq 0 ]; then
+  test_overcloud
+  rc=$?
+fi
 exit $rc
