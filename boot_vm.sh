e156b3d21ed7c25080bff2d85f2182f26427d7ea
David Hill <dhill@redhat.com>
2016-01-29 17:28:43 -0500

    Adding net-id= in --nic definition of boot_vm.sh

--- boot_vm.sh
+++ boot_vm.sh
@@ -3,4 +3,4 @@
 image=$( glance image-list | grep cirros | head -1 | awk '{ print $2 }')
 neutron=$( neutron net-list | grep test | awk '{ print $2 }')
 
-nova boot --flavor m1.tiny --image $image  --nic $neutron test
+nova boot --flavor m1.tiny --image $image  --nic net-id=$neutron test
