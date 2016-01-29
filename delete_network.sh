2dfd11fa16cf35307f85b19c0454d042e958c161
David Hill <dhill@redhat.com>
2016-01-29 18:10:00 -0500

    Waiting for neutron to actually delete the net/subnet in delete_network.sh

--- delete_network.sh
+++ delete_network.sh
@@ -3,4 +3,17 @@
 source setup.cfg
 
 neutron subnet-delete test-subnet
+op=$( neutron subnet-list | grep test-subnet )
+while [ ! -z "$op" ]; do
+  op=$( neutron subnet-list | grep test-subnet )
+  echo -n "."
+done
+
 neutron net-delete test
+op=$( neutron net-list | grep test )
+while [ ! -z "$op" ]; do
+  op=$( neutron net-list | grep test )
+  echo -n "."
+done
+
+exit 0
