4f5ebd6283d09e2333a53c2dc37f95e589ba0633
David Hill <dhill@redhat.com>
2016-01-29 17:27:17 -0500

    Fixing net-create in create_network.sh

--- create_network.sh
+++ create_network.sh
@@ -2,5 +2,5 @@
 
 source setup.cfg
 
-neutron net-create --provider:network_type vlan $neutronnwtype
+neutron net-create --provider:network_type  $neutronnwtype test
 neutron subnet-create test 10.254.0.0/16 --name test-subnet
