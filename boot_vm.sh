f9a596eb822284395a6c53721d1aa90e461f4fa8
David Hill <dhill@redhat.com>
2016-01-29 19:27:16 -0500

    We delete test-vm but we create test.  Fixing this in boot_vm.sh

--- boot_vm.sh
+++ boot_vm.sh
@@ -6,7 +6,7 @@ image=$( glance image-list | grep cirros | head -1 | awk '{ print $2 }')
 neutron=$( neutron net-list | grep test | awk '{ print $2 }')
 
 echo "Creating test VM..."
-nova boot --flavor m1.tiny --image $image  --nic net-id=$neutron test
+nova boot --flavor m1.tiny --image $image  --nic net-id=$neutron test-vm
 
 state=$(nova list | grep test-vm | awk '{ print $6 }')
 while [[ ! "$state" =~ ACTIVE ]] && [[ ! "$state" =~ FAILED ]]; do
