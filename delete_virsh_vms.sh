e327ef1ab0c2254424fe3638aab9c1c2a08bfc06
David Hill <dhill@redhat.com>
2016-02-04 18:45:51 -0500

    Making sure we use the proper logic when deleting remotely/locally the VMs.

--- delete_virsh_vms.sh
+++ delete_virsh_vms.sh
@@ -13,11 +13,17 @@ function delete_vms {
     max=$computescale
   fi
   while [ $inc -lt $max ]; do
-    output=$(sudo "virsh list" | grep "$type-$inc")
+    output=$(sudo virsh list | grep "$type-$inc")
     if [[ "$output" =~ $type-$inc ]]; then 
       virsh undefine $type-$inc
     else
-      ssh root@$kvmhost "virsh undefine $type-$inc"
+      ip=$(ip addr)
+      if [[ ! "$kvmhost" =~ $ip ]]; then
+        output=$(ssh stack@$kvmhost "sudo virsh list | grep $type-$inc")
+        if [[ "$output" =~ $type-$inc ]]; then
+          ssh stack@$kvmhost "sudo virsh undefine $type-$inc"
+        fi
+      fi
     fi
     inc=$(expr $inc + 1)
   done
