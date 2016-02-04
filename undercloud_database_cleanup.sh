da286de5df5ca884a73cbbda0d669d3ff5768336
David Hill <dhill@redhat.com>
2016-02-04 18:09:18 -0500

    Fixing undercloud_database_cleanup.sh

--- undercloud_database_cleanup.sh
+++ undercloud_database_cleanup.sh
@@ -3,12 +3,12 @@
 table_list="block_device_mapping; instance_actions_events instance_actions  instance_extra instance_info_caches instance_system_metadata instance_faults instances" 
 
 for table in $table_list; do
-    echo "delete from ${table};" | ${mysql} -D nova
+    echo "delete from ${table};" | mysql -D nova
 done
 
 table_list="ports nodes"
 
 for table in $table_list; do
-    echo "delete from ${table};" | ${mysql} -D ironic
+    echo "delete from ${table};" | mysql -D ironic
 done
 
