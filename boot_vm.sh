04ce26d35dadc81d1250cc264dcf7ef54705d6ba
David Hill <dhill@redhat.com>
2016-02-04 16:29:08 -0500

    Failed VMs are in ERROR state.

--- boot_vm.sh
+++ boot_vm.sh
@@ -9,7 +9,7 @@ echo "Creating test VM..."
 nova boot --flavor m1.tiny --image $image  --nic net-id=$neutron test-vm
 
 state=$(nova list | grep test-vm | awk '{ print $6 }')
-while [[ ! "$state" =~ ACTIVE ]] && [[ ! "$state" =~ FAILED ]]; do
+while [[ ! "$state" =~ ACTIVE ]] && [[ ! "$state" =~ ERROR ]]; do
   state=$(nova list | grep test-vm | awk '{ print $6 }')
   echo -n .
 done
