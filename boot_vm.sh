04c1e9c3a9b8ddebbffc2571e96868243aa2b290
David Hill <dhill@redhat.com>
2016-02-04 16:34:13 -0500

    Creating m1.micro flavor for test in boot_vm.sh

--- boot_vm.sh
+++ boot_vm.sh
@@ -5,8 +5,10 @@ rc=255
 image=$( glance image-list | grep cirros | head -1 | awk '{ print $2 }')
 neutron=$( neutron net-list | grep test | awk '{ print $2 }')
 
+echo "Creating m1.micro VM..."
+nova flavor-create m1.micro auto 256 1 1
 echo "Creating test VM..."
-nova boot --flavor m1.tiny --image $image  --nic net-id=$neutron test-vm
+nova boot --flavor m1.micro --image $image  --nic net-id=$neutron test-vm
 
 state=$(nova list | grep test-vm | awk '{ print $6 }')
 while [[ ! "$state" =~ ACTIVE ]] && [[ ! "$state" =~ ERROR ]]; do
