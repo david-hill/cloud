cf3eba2a4d5d7211a742b2779301fcad85fdc392
David Hill <dhill@redhat.com>
2016-02-09 18:01:13 -0500

    Adding external subnet to router in create_network.sh

--- create_network.sh
+++ create_network.sh
@@ -9,3 +9,4 @@ neutron router-gateway-set test-router ext-net
 neutron net-create --provider:network_type  $neutronnwtype test
 neutron subnet-create test 10.254.0.0/16 --name test-subnet
 neutron router-interface-add test-router test-subnet
+neutron router-interface-add test-router ext-subnet
