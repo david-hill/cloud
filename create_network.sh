15905010db6fcc77a06a8e688b74b465695a124c
David Hill <dhill@redhat.com>
2016-02-09 19:44:25 -0500

    Allow now gateways in create_network.sh.

--- create_network.sh
+++ create_network.sh
@@ -2,9 +2,14 @@
 
 source setup.cfg
 
+if [ ! -z "$gw" ]; then
+  gwarg="--gateway $gw"
+else
+  gwarg=""
+fi
 neutron router-create test-router
 neutron net-create ext-net --router:external True --provider:physical_network datacentre --provider:network_type flat
-neutron subnet-create --name ext-subnet --allocation-pool start=10.1.2.60,end=10.1.2.70 --disable-dhcp --gateway 10.1.2.1 ext-net 10.1.2.0/24
+neutron subnet-create --name ext-subnet --allocation-pool start=10.1.2.60,end=10.1.2.70 --disable-dhcp $gwarg ext-net 10.1.2.0/24
 neutron router-gateway-set test-router ext-net
 neutron net-create --provider:network_type  $neutronnwtype test
 neutron subnet-create test 10.254.0.0/16 --name test-subnet
