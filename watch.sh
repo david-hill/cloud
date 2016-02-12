86b3c06e159333e30513b621111a2eaaf6b78cf8
David Hill <dhill@redhat.com>
2016-02-11 19:49:18 -0500

    ADding function source_rc in functions and using it in watch.sh

--- watch.sh
+++ watch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-source /home/stack/stackrc
+source functions
 
-watch 'ironic node-list && nova list && heat resource-list -n10 overcloud | grep -vi complete'
+source_rc /home/stack/stackrc
+if [ $? -eq 0 ]; then
+  watch 'ironic node-list && nova list && heat resource-list -n10 overcloud | grep -vi complete'
+fi
