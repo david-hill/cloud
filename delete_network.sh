8aa57f1fd060951840ad5cc714b134499fb4db6d
David Hill <dhill@redhat.com>
2016-02-11 20:10:34 -0500

    Sourcing overcloudrc in delete_network.sh

--- delete_network.sh
+++ delete_network.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 source setup.cfg
+source functions
 
-neutron router-interface-delete test-router test-subnet
-neutron router-gateway-clear test-router
+source_rc overcloudrc
+if [ $? -eq 0 ]; then
+  neutron router-interface-delete test-router test-subnet
+  neutron router-gateway-clear test-router
 
-neutron subnet-delete test-subnet
-op=$( neutron subnet-list | grep test-subnet )
-while [ ! -z "$op" ]; do
+  neutron subnet-delete test-subnet
   op=$( neutron subnet-list | grep test-subnet )
-  echo -n "."
-done
+  while [ ! -z "$op" ]; do
+    op=$( neutron subnet-list | grep test-subnet )
+    echo -n "."
+  done
 
-neutron net-delete test
-op=$( neutron net-list | grep test )
-while [ ! -z "$op" ]; do
+  neutron net-delete test
   op=$( neutron net-list | grep test )
-  echo -n "."
-done
+  while [ ! -z "$op" ]; do
+    op=$( neutron net-list | grep test )
+    echo -n "."
+  done
 
-neutron subnet-delete ext-subnet
-neutron net-delete ext-net
+  neutron subnet-delete ext-subnet
+  neutron net-delete ext-net
 
-neutron router-delete test-router
+  neutron router-delete test-router
+fi
 exit 0
