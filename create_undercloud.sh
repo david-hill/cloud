840e3dd6e65187a89cae5f327320a6a8b0c827b0
David Hill <dhill@redhat.com>
2016-02-13 01:18:39 -0500

    Adding create_undercloud.sh

---
+++ create_undercloud.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source functions
+source setup.cfg
+source creds.cfg
+if [ -e "creds.cfg.local" ]; then
+  source creds.cfg.local
+fi
+
+validate_env
+if [ $? -eq 0 ]; then
+  memory=$undercloudmemory
+  type=undercloud
+  inc=rhosp8
+  sudo cp /home/dhill/VMs/rhel-guest-image-7.2-20151102.0.x86_64.qcow2 /home/dhill/VMs/undercloud-rhosp8.qcow2
+  sudo qemu-img resize /home/dhill/VMs/undercloud-rhosp8.qcow2 30G
+  sudo virt-customize -a /home/dhill/VMs/undercloud-rhosp8.qcow2 --copy-in customize.service:/etc/systemd/system/ 
+  sudo virt-customize -a /home/dhill/VMs/undercloud-rhosp8.qcow2 --copy-in S01customize:/etc/rc.d/rc3.d/ --root-password password:$rootpasswd
+  sudo virt-customize -a /home/dhill/VMs/undercloud-rhosp8.qcow2 --link /etc/systemd/system/customize.service:/etc/systemd/system/multi-user.target.wants/customize.service
+
+  tmpfile=$(mktemp)
+  uuid=$(uuidgen)
+  tpath='/home/dhill/VMs/undercloud-rhosp8.qcow'
+  gen_macs
+  gen_xml
+  create_domain
+  cleanup
+
+else
+  echo "Please run this on baremetal..."
+fi
