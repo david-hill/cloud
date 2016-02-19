d15991f52dd048a33fb672c1bb84bfbe6bd97bbf
David Hill <dhill@redhat.com>
2016-02-18 20:33:09 -0500

    Fixing delete_undercloud.sh

---
+++ delete_undercloud.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source functions
+source_rc setup.cfg
+
+function delete_vms {
+  type=$1
+  inc=0
+  output=$(sudo virsh list --all | grep "$type-$inc-$releasever")
+  sudo virsh undefine $type-$inc-$releasever
+}
+
+function cleanup {
+  if [ -e /home/stack/instackenv.json-$releasever ]; then
+    sudo rm -rf /home/stack/instackenv.json-$releasever
+  fi
+}
+delete_vms undercloud
+cleanup
