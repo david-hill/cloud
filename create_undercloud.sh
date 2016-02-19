f4041166f0e2801f6ebf21a08a9f86203d63a881
David Hill <dhill@redhat.com>
2016-02-19 00:05:06 -0500

    Adding some verbosity in create_undercloud.sh.

--- create_undercloud.sh
+++ create_undercloud.sh
@@ -21,11 +21,16 @@ if [ $? -eq 0 ]; then
   sed -i "s/rhosp8/$releasever/g" tmp/S01customize
   vpnip=$(ip addr | grep inet | grep 10 | awk ' { print $2 }' | sed -e 's#/32##')
   sudo iptables -t nat -I POSTROUTING -s 192.168.122.0/24 -d 10.0.0.0/8 -o wlp3s0 -j SNAT --to-source $vpnip
+  echo "Copying base image..."
   sudo cp /home/dhill/VMs/rhel-guest-image-7.2-20151102.0.x86_64.qcow2 /home/dhill/VMs/${vmname}.qcow2
-  sudo qemu-img resize /home/dhill/VMs/${vmname}.qcow2 30G
-  sudo virt-customize -a /home/dhill/VMs/${vmname}.qcow2 --copy-in customize.service:/etc/systemd/system/ 
-  sudo virt-customize -a /home/dhill/VMs/${vmname}.qcow2 --copy-in tmp/S01customize:/etc/rc.d/rc3.d/ --root-password password:$rootpasswd
-  sudo virt-customize -a /home/dhill/VMs/${vmname}.qcow2 --link /etc/systemd/system/customize.service:/etc/systemd/system/multi-user.target.wants/customize.service
+  echo "Resizing base disk..."
+  sudo qemu-img resize /home/dhill/VMs/${vmname}.qcow2 30G > /dev/null
+  echo "Copying customize.service into image..."
+  sudo virt-customize -a /home/dhill/VMs/${vmname}.qcow2 --copy-in customize.service:/etc/systemd/system/  > /dev/null
+  echo "Creating root password and copying S01customize..."
+  sudo virt-customize -a /home/dhill/VMs/${vmname}.qcow2 --copy-in tmp/S01customize:/etc/rc.d/rc3.d/ --root-password password:$rootpasswd > /dev/null
+  echo "Enabling customize.service into systemd..."
+  sudo virt-customize -a /home/dhill/VMs/${vmname}.qcow2 --link /etc/systemd/system/customize.service:/etc/systemd/system/multi-user.target.wants/customize.service > /dev/null
 
   tmpfile=$(mktemp)
   uuid=$(uuidgen)
@@ -36,15 +41,22 @@ if [ $? -eq 0 ]; then
   start_domain
   cleanup
 
+  echo -n "Waiting for VM to come up..."
   down=1
   while [ $down -eq 1 ]; do
-    ping -c 1 192.168.122.2
+    echo -n "."
+    ping -c 1 192.168.122.2 > /dev/null
     down=$?
+    sleep 1
   done
+  echo -n "Waiting for SSH to come up..."
   sshrc=1
+  ssh-keygen -R 192.168.122.2
   while [ $ssh -ne 0 ]; do
-    ssh stack@192.168.122.2 'uptime'
+    echo -n "."
+    ssh stack@192.168.122.2 'uptime' > /dev/null
     sshrc=$?
+    sleep 1
   done
   bash create_virsh_vms.sh
 
