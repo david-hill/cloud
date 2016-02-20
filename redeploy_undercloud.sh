778c6c1fa785525ace92ac3f7a2760731333e168
David Hill <dhill@redhat.com>
2016-02-20 16:49:53 -0500

    Sending all outputs to /dev/null in redeploy_undercloud.sh

--- redeploy_undercloud.sh
+++ redeploy_undercloud.sh
@@ -14,20 +14,20 @@ function cleanup_logs {
 function cleanup_undercloud {
   echo "Uninstalling undercloud..."
   rm overcloudrc
-  sudo yum remove -y openstack-* python-oslo-*
-  sudo yum remove -y mariadb
+  sudo yum remove -y openstack-* python-oslo-* > /dev/null
+  sudo yum remove -y mariadb > /dev/null
   sudo rm -rf /var/lib/mysql
   sudo rm -rf /var/lib/ironic-discoverd/discoverd.sqlite
-  sudo yum install -y python-rdomanager-oscplugin
+  sudo yum install -y python-rdomanager-oscplugin > /dev/null
   cleanup_logs
 }
 
 function conformance {
-  sudo yum update -y
-  sudo yum install -y ntpdate ntp screen libguestfs-tools wget vim
-  sudo service ntpd stop
-  sudo ntpdate $ntpserver
-  sudo service ntpd start
+  sudo yum update -y > /dev/null
+  sudo yum install -y ntpdate ntp screen libguestfs-tools wget vim > /dev/null
+  sudo service ntpd stop > /dev/null
+  sudo ntpdate $ntpserver > /dev/null
+  sudo service ntpd start > /dev/null
 }
 
 function create_flavors {
@@ -56,12 +56,12 @@ function create_flavors {
     bram=4095
   fi
   for profile in swift-storage block-storage control ceph-storage compute; do
-    openstack flavor delete $profile
-    openstack flavor create --id auto --ram $ram --disk $disk --vcpus $vcpus --swap $swap $profile
-    openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="$profile" $profile
+    openstack flavor delete $profile > /dev/null
+    openstack flavor create --id auto --ram $ram --disk $disk --vcpus $vcpus --swap $swap $profile > /dev/null
+    openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="$profile" $profile > /dev/null
   done
-  openstack flavor delete baremetal
-  openstack flavor create --id auto --ram $bram --disk $disk --vcpus $vcpus --swap $swap baremetal
+  openstack flavor delete baremetal > /dev/null
+  openstack flavor create --id auto --ram $bram --disk $disk --vcpus $vcpus --swap $swap baremetal > /dev/null
 }
 
 function tag_hosts {
@@ -69,11 +69,11 @@ function tag_hosts {
   inc=0
   for p in $(ironic node-list | grep available | awk '{ print $2 }'); do
     if [ $inc -lt 3 -a $controlscale -eq 3 ] || [ $controlscale -eq 1 -a $inc -lt 1 ]; then
-      ironic node-update $p add properties/capabilities='profile:control,boot_option:local'
+      ironic node-update $p add properties/capabilities='profile:control,boot_option:local' > /dev/null
     elif [ $inc -lt 6 -a $cephscale -gt 0 ]; then
-      ironic node-update $p add properties/capabilities='profile:ceph-storage,boot_option:local'
+      ironic node-update $p add properties/capabilities='profile:ceph-storage,boot_option:local' > /dev/null
     else
-      ironic node-update $p add properties/capabilities='profile:compute,boot_option:local'
+      ironic node-update $p add properties/capabilities='profile:compute,boot_option:local' > /dev/null
     fi
     inc=$( expr $inc + 1)
   done
@@ -81,17 +81,17 @@ function tag_hosts {
 
 function create_oc_images {
   echo "Importing overcloud images..."
-  openstack overcloud image upload --image-path /home/stack/images
+  openstack overcloud image upload --image-path /home/stack/images > /dev/null
 }
 
 function baremetal_setup {
   echo "Configure baremetal hosts..."
   echo "Importing instackenv.json..."
-  openstack baremetal import --json /home/stack/instackenv.json
+  openstack baremetal import --json /home/stack/instackenv.json > /dev/null
   echo "Configure node boot..."
-  openstack baremetal configure boot
+  openstack baremetal configure boot > /dev/null
   echo "Starting introspection..."
-  openstack baremetal introspection bulk start
+  openstack baremetal introspection bulk start > /dev/null
 }
 
 
@@ -117,13 +117,13 @@ function deploy_overcloud {
 
 function install_undercloud {
   echo "Installing undercloud ..."
-  openstack undercloud install
+  openstack undercloud install > /dev/null
 }
 
 function validate_network_environment {
   echo "Validating network environment..."
-  git clone https://github.com/rthallisey/clapper
-  python clapper/network-environment-validator.py -n ../$releasever/network-environment.yaml
+  git clone https://github.com/rthallisey/clapper > /dev/null
+  python clapper/network-environment-validator.py -n ../$releasever/network-environment.yaml > /dev/null
   rc=$?
   return $rc
 }
@@ -131,7 +131,7 @@ function validate_network_environment {
 function delete_nova_nodes {
   echo "Deleting nova nodes.."
   for node in $(nova list | awk '{ print $2 }' | grep -v ID); do
-    nova delete $node
+    nova delete $node > /dev/null
   done
   if [ ! -z "$node" ]; then
     tnode=$(nova list | grep $node)
@@ -144,7 +144,7 @@ function delete_nova_nodes {
 function poweroff_ironic_nodes {
   echo "Powering off ironic nodes.."
   for node in $(ironic node-list | grep "power on" | awk '{ print $2 }'); do
-    ironic node-set-power-state $node off
+    ironic node-set-power-state $node off > /dev/null
     tnode=$(ironic node-list | grep $node | grep "power on")
     while [[ "$tnode" =~ $node ]]; do
       tnode=$(ironic node-list | grep $node | grep "power on")
@@ -155,7 +155,7 @@ function poweroff_ironic_nodes {
 function delete_ironic_nodes {
   echo "Deleting ironic nodes.."
   for node in $(ironic node-list | egrep "True|False" | awk '{ print $2 }'); do
-    ironic node-delete $node
+    ironic node-delete $node > /dev/null
     tnode=$(ironic node-list | grep $node)
     while [[ "$tnode" =~ $node ]]; do
       tnode=$(ironic node-list | grep $node)
