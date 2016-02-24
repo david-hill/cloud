572789fcb70e441411504fcf2883f12b1fe162b7
David Hill <dhill@redhat.com>
2016-02-24 14:51:48 -0500

    Verbose in boot_vm.sh, create_network.sh and delete_network.sh

--- delete_network.sh
+++ delete_network.sh
@@ -5,26 +5,41 @@ source functions
 
 source_rc overcloudrc
 if [ $? -eq 0 ]; then
-  neutron router-interface-delete test-router test-subnet
-  neutron router-gateway-clear test-router
+  startlog "Removing interface from router"
+  neutron router-interface-delete test-router test-subnet > /dev/null
+  endlog "done"
+  startlog "Clearing router gateway"
+  neutron router-gateway-clear test-router > /dev/null
+  endlog "done"
 
-  neutron subnet-delete test-subnet
+  startlog "Deleting test subnet"
+  neutron subnet-delete test-subnet > /dev/null
   op=$( neutron subnet-list | grep test-subnet )
   while [ ! -z "$op" ]; do
     op=$( neutron subnet-list | grep test-subnet )
     echo -n "."
   done
+  endlog "done"
 
-  neutron net-delete test
+  startlog "Deleting test network"
+  neutron net-delete test > /dev/null
   op=$( neutron net-list | grep test )
   while [ ! -z "$op" ]; do
     op=$( neutron net-list | grep test )
     echo -n "."
   done
+  endlog "done"
 
-  neutron subnet-delete ext-subnet
-  neutron net-delete ext-net
+  startlog "Deleting external subnet"
+  neutron subnet-delete ext-subnet > /dev/null
+  endlog "done"
 
-  neutron router-delete test-router
+  startlog "Deleting external network"
+  neutron net-delete ext-net > /dev/null
+  endlog "done"
+
+  startlog "Deleting router"
+  neutron router-delete test-router > /dev/null
+  endlog "done"
 fi
 exit 0
