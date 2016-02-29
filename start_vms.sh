82e73f7ea1b0d57e8c0cba5f4960bc8b237007b5
David Hill <dhill@redhat.com>
2016-02-29 16:19:36 -0500

    Make start_vms.sh use setup.cfg and operate only on those VMs.

--- start_vms.sh
+++ start_vms.sh
@@ -1,3 +1,6 @@
-for p in $(sudo virsh list --all | grep shut | awk '{ print $2 }'); do 
+source functions
+source_rc setup.cfg
+
+for p in $(sudo virsh list --all | grep shut | grep $releasever | awk '{ print $2 }'); do 
   sudo virsh start $p
 done
