81562ea40409cbd747d8c7e67c291a4f7131a38d
David Hill <dhill@redhat.com>
2016-03-01 18:22:37 -0500

    Showing the deployment, if it's one, in analyze_failure.sh

--- analyze_failure.sh
+++ analyze_failure.sh
@@ -5,7 +5,9 @@ source_rc /home/stack/stackrc
 
 for stack in $(heat stack-list | grep -i failed | awk '{ print $2 }'); do 
   for nstack in $(heat stack-list --show-nested | grep stack | grep -i failed | awk '{ print $2 }'); do
-    for resource in $( heat resource-list $nstack | grep -i failed | awk '{ print $2 }'); do
+    for resource in $( heat resource-list -n10 $nstack | grep -i failed | awk '{ print $2 }'); do
+      deployment=$(heat resource-list -n10 $nstack | grep -i $resource | grep Deployment | awk '{ print $2 }')
+      heat deployment-show $deployment
     done
   done
 done
