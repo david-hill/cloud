57acebf385c679b459ec9beaa5aff49aef70c68b
David Hill <dhill@redhat.com>
2016-03-10 19:40:40 -0500

    Verbose in delete_overcloud.sh

--- delete_overcloud.sh
+++ delete_overcloud.sh
@@ -4,7 +4,7 @@ source functions
 source setup.cfg
 
 function delete_nova_nodes {
-  echo "Deleting nova nodes.."
+  startlog "Deleting nova nodes"
   for node in $(nova list | awk '{ print $2 }' | grep -v ID); do
     nova delete $node
   done
@@ -12,36 +12,35 @@ function delete_nova_nodes {
     tnode=$(nova list | grep $node)
     while [[ "$tnode" =~ $node ]]; do
       tnode=$(nova list | grep $node)
-      echo -n "."
     done
   fi
+  endlog "done"
 }
 
 function poweroff_ironic_nodes {
-  echo "Powering off ironic nodes.."
+  starlog "Powering off ironic nodes"
   for node in $(ironic node-list | grep "power on" | awk '{ print $2 }'); do
     ironic node-set-power-state $node off
     tnode=$(ironic node-list | grep $node | grep "power on")
     while [[ "$tnode" =~ $node ]]; do
       tnode=$(ironic node-list | grep $node | grep "power on")
-      echo -n "."
     done
   done
+  endlog "done"
 }
 
 function delete_ironic_nodes {
-  echo "Deleting ironic nodes.."
+  startlog "Deleting ironic nodes.."
   for node in $(ironic node-list | egrep "True|False" | awk '{ print $2 }'); do
     ironic node-delete $node
     tnode=$(ironic node-list | grep $node)
     while [[ "$tnode" =~ $node ]]; do
       tnode=$(ironic node-list | grep $node)
-      echo -n "."
     done
   done
+  endlog "done"
 }
 function delete_nodes {
-  echo "Deleting nodes..."
   delete_nova_nodes
   poweroff_ironic_nodes
   delete_ironic_nodes
