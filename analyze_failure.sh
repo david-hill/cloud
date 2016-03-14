6010a832f3d937f379ffd00fd72e5a04de21e7eb
David Hill <dhill@redhat.com>
2016-03-14 17:35:01 -0400

    Fixing typo in analyze_failure.sh

--- analyze_failure.sh
+++ analyze_failure.sh
@@ -4,7 +4,7 @@ source setup.cfg
 source_rc /home/stack/stackrc
 
 for stack in $(heat stack-list | grep -i failed | awk '{ print $2 }'); do 
-  for nstack in $(heat stack-list --show-nested | grep stack | grep -i failed | awk '{ print $2 }'); do
+  for nstack in $(heat stack-list --show-nested | grep $stack | grep -i failed | awk '{ print $2 }'); do
     for resource in $( heat resource-list -n10 $nstack | grep -i failed | awk '{ print $2 }'); do
       deployment=$(heat resource-list -n10 $nstack | grep -i $resource | grep Deployment | awk '{ print $2 }')
       heat deployment-show $deployment
