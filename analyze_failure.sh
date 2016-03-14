cf909427caa95993957b451c415ea459593deadc
David Hill <dhill@redhat.com>
2016-03-14 17:43:09 -0400

    Do nothing if $deployment is not set in analyze_failure.sh

--- analyze_failure.sh
+++ analyze_failure.sh
@@ -6,8 +6,10 @@ source_rc /home/stack/stackrc
 for stack in $(heat stack-list | grep -i failed | awk '{ print $2 }'); do 
   for nstack in $(heat stack-list --show-nested | grep $stack | grep -i failed | awk '{ print $2 }'); do
     for resource in $( heat resource-list -n10 $nstack | grep -i failed | awk '{ print $2 }'); do
-      deployment=$(heat resource-list -n10 $nstack | grep -i $resource | grep Deployment | awk '{ print $2 }')
-      heat deployment-show $deployment
+      deployment=$(heat resource-list -n10 $nstack | grep -i $resource | grep Deployment | awk '{ print $4 }')
+      if [ ! -z "$deployment"  ]; then
+        heat deployment-show $deployment
+      fi
     done
   done
 done
