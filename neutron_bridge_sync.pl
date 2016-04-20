98402e64a53ad3db9805b75746c3577ac4eeabef
David Hill <dhill@redhat.com>
2016-04-20 17:54:16 -0400

    Adding neutron_bridge_sync.pl

---
+++ neutron_bridge_sync.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+my %usedbridgelist;
+#  ovs-vsctl list-ports br-int
+open READ, "cat /etc/libvirt/qemu/*.xml | grep "qbr" | awk -F \' '{ print $2 }' |";
+while (<READ>) {
+  if ($_ =~ /qbr/) {
+		@split = split /'/,$_;
+		my $bridge = $split[1];
+		$usedbridgelist{$bridge}++;
+  }
+}
+open READ, "brctl show |";
+while (<READ>) {
+	chop $_;
+	if ($_ =~ /qbr/) {
+		my @split = split /\t/, $_;
+		$presentbridge{$split[0]}=$split[5];
+	}
+}
+foreach my $bridge (sort keys %presentbridge) {
+	if ($usedbridgelist{$bridge}) {
+		print "keeping $bridge\n";
+	} else {
+		`ifconfig $bridge down`;
+		`brctl delif $bridge $presentbridge{$bridge}`;
+		`brctl delbr $bridge`;
+		print "Deleting $bridge and $presentbridge{$bridge}\n";
+		my $ovsbridge=$bridge;
+		$ovsbridge=~s/qbr/qvo/g;
+		`ovs-vsctl del-port $ovsbridge`;
+	}
+}
