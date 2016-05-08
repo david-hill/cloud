30258d0e714e0a48ecaada285dd2e588d5670693
David Hill <dhill@redhat.com>
2016-05-08 18:06:04 -0400

    Grep for failed deployments in analyze_failure.sh

--- analyze_failure.sh
+++ analyze_failure.sh
@@ -6,7 +6,7 @@ source_rc /home/stack/stackrc
 for stack in $(heat stack-list | grep -i failed | awk '{ print $2 }'); do 
   for nstack in $(heat stack-list --show-nested | grep $stack | grep -i failed | awk '{ print $2 }'); do
     for resource in $( heat resource-list -n10 $nstack | grep -i failed | awk '{ print $2 }'); do
-      deployments=$(heat resource-list -n10 $nstack | grep -i $resource | grep Deployment | awk '{ print $4 }')
+      deployments=$(heat resource-list -n10 $nstack | grep -i failed | grep -i $resource | grep Deployment | awk '{ print $4 }')
       if [ ! -z "$deployments"  ]; then
         for deployment in $deployments; do
           heat deployment-show $deployment
