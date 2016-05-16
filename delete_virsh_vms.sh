ffae8c4b6f718f2020f352a09cebee50a9c1226e
David Hill <dhill@redhat.com>
2016-05-16 12:54:05 -0400

    Sending outputs to $stderr and $stdout in delete_virsh_vms.sh

--- delete_virsh_vms.sh
+++ delete_virsh_vms.sh
@@ -17,17 +17,17 @@ function delete_vms {
     output=$(sudo virsh list --all | grep "$type-$inc-$releasever")
     if [[ "$output" =~ $type-$inc ]]; then 
       for snap in $(sudo virsh snapshot-list $type-$inc-$releasever | egrep "running|shut off" | awk '{ print $1 }'); do
-        sudo virsh snapshot-delete $type-$inc-$releasever $snap > /dev/null
+        sudo virsh snapshot-delete $type-$inc-$releasever $snap 2>$stderr 1>$stdout
       done
-      sudo virsh destroy $type-$inc-$releasever > /dev/null
-      sudo virsh undefine $type-$inc-$releasever > /dev/null
+      sudo virsh destroy $type-$inc-$releasever 2>$stderr 1>$stdout
+      sudo virsh undefine $type-$inc-$releasever 2>$stderr 1>$stdout
     else
       ip=$(ip addr)
       if [[ ! "$ip" =~ $kvmhost ]]; then
         output=$(ssh root@$kvmhost "sudo virsh list --all | grep $type-$inc-$releasever")
         if [[ "$output" =~ $type-$inc-$releaserver ]]; then
-          ssh root@$kvmhost "sudo virsh destroy $type-$inc-$releasever" > /dev/null
-          ssh root@$kvmhost "sudo virsh undefine $type-$inc-$releasever" > /dev/null
+          ssh root@$kvmhost "sudo virsh destroy $type-$inc-$releasever" 2>$stderr 1>$stdout
+          ssh root@$kvmhost "sudo virsh undefine $type-$inc-$releasever" 2>$stderr 1>$stdout
         fi
       fi
     fi
