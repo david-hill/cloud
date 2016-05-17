668660341e3ee1011327e7f1a3c1efe2933d4cab
David Hill <dhill@redhat.com>
2016-05-17 11:37:00 -0400

    Sending ssh output to $stderr and $stdout

--- delete_undercloud.sh
+++ delete_undercloud.sh
@@ -7,7 +7,7 @@ function delete_vms {
   type=$1
   inc=0
   output=$(sudo virsh list --all | grep "$type-$inc-$releasever")
-  ssh stack@$undercloudip 'sudo subscription-manager unregister'
+  ssh stack@$undercloudip 'sudo subscription-manager unregister' 2>$stderr 1>$stdout
   for snap in $(sudo virsh snapshot-list $type-$inc-$releasever | egrep "shut off|running" | awk '{ print $1 }'); do
     sudo virsh snapshot-delete $type-$inc-$releasever $snap 2>$stderr 1>$stdout
   done
