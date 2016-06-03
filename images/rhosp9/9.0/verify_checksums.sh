2e191c684fe82f7265140b582a67234b914e6352
David Hill <dhill@redhat.com>
2016-06-03 11:09:55 -0400

    Adding image synching scripts.

---
+++ images/rhosp9/9.0/verify_checksums.sh
@@ -0,0 +1,31 @@
+url=http://rhos-release.virt.bos.redhat.com/puddle-images/9.0/
+
+rm -rf index.html
+wget -q $url
+for folder in $(cat index.html | grep DIR | awk -F\" '{ print $8 }'); do
+  if [[ $folder =~ test ]]; then
+    continue;
+  fi
+  if [ -e CHECKSUM ]; then
+    rm -rf CHECKSUM
+  fi
+  wget -q $url/${folder}images/CHECKSUM
+  folder=`echo $folder | sed -e 's/\///' -e 's/^/9.0-/'`
+  if [ -e backup/$folder ]; then
+    for file in $(ls backup/$folder); do
+      sum=$(md5sum backup/$folder/$file | awk '{ print $1 }')
+      grep $file CHECKSUM | grep -q $sum
+      if [ $? -ne 0 ]; then
+        echo backup/$folder/$file has different checksum
+      else
+        echo backup/$folder/$file with $sum is same
+      fi
+    done
+  fi
+done 
+
+
+
+
+
+
