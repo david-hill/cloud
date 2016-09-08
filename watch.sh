ae18edebe954b745b4e2ed961004b5f2efdb82f0
David Hill <dhill@redhat.com>
2016-09-08 12:49:21 -0400

    Permit variable expension in watch.sh

--- watch.sh
+++ watch.sh
@@ -4,6 +4,7 @@ source functions
 
 source_rc /home/stack/stackrc
 source_rc setup.cfg
+
 if [ $? -eq 0 ]; then
-  watch 'ironic node-list && nova list && heat resource-list -n10 $overcloudname | grep -vi complete'
+  watch "ironic node-list && nova list && heat resource-list -n10 $overcloudname | grep -vi complete"
 fi
