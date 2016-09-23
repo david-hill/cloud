940c0379faebc958197ab5d458c87c2f8663a483
Jenkins Continuous Build server <jenkins@zappa.orion>
2016-09-23 10:54:07 -0400

    Adding jenkinspath to setup.cfg.

--- merge.sh
+++ merge.sh
@@ -1,4 +1,5 @@
 commit=$(git log | grep commit | head -1 | awk '{ print $2 }' )
+commit=a83d6e91afb7a30b3bcbe2a9102db334ae953c7c
 
 for p in $(git branch  | awk '{ print $1 }' | grep -v '\*'); do
   echo $p
