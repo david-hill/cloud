a6af542641b4a121014ec8281b56349588a9d450
David Hill <dhill@redhat.com>
2016-09-25 20:50:22 -0400

    Adding rhosp10/update_images.sh

---
+++ images/rhosp10/update_images.sh
@@ -0,0 +1,54 @@
+function makefolders {
+  rc=0
+  if [ ! -e backup ]; then
+    mkdir backup
+    rc=$?
+  fi
+  if [ $rc -eq 0 ]; then
+    if [ ! -e tmp ]; then
+      mkdir tmp
+      rc=$?
+    fi
+  fi
+  return $rc
+}
+
+function backup_images {
+  file=$1
+  curdate=$( date +'%Y%m%d%H' )
+  if [ ! -d ../backup/$curdate ]; then
+    mkdir -p ../backup/$curdate
+  fi
+  mv ../$file ../backup/$curdate
+  mv ../$file.md5 ../backup/$curdate
+}
+function get_images {
+  rc=255
+  wpath=$1
+  makefolders
+  rc=$?
+  if [ $rc -eq 0 ]; then
+    cd tmp
+    for file in $files; do
+      wget -q ${wpath}/$file.md5 -O $file.md5
+      if [ -e ../$file.md5 ]; then
+        pver=$(cat ../$file.md5)
+        cver=$(cat $file.md5)
+        if [ ! "$pver" == "$cver" ]; then
+          backup_images $file
+          cp $file.md5 ../
+          wget -q ${wpath}/$file -O ../$file
+        fi
+      else
+        cp $file.md5 ../
+        wget -q ${wpath}/$file -O $file
+      fi
+    done
+    cd ..
+  fi
+  return $rc
+}
+
+files="ironic-python-agent.tar overcloud-full.tar"
+get_images http://rhos-release.virt.bos.redhat.com/poodle-images/rhos-10/current-passed-ci/
+exit $?
