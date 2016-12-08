97cc01a052f65d2d531466cd2ebc9529a8ce7940
David Hill <dhill@redhat.com>
2016-12-08 12:44:35 -0500

    Fixing neutron_bridge_sync.pl

--- neutron_bridge_sync.pl
+++ neutron_bridge_sync.pl
@@ -1,32 +1,36 @@
 #!/usr/bin/perl
 my %usedbridgelist;
+my $debug=1;
 #  ovs-vsctl list-ports br-int
-open READ, "cat /etc/libvirt/qemu/*.xml | grep "qbr" | awk -F \' '{ print $2 }' |";
+open READ, "cat /etc/libvirt/qemu/*.xml  |grep qbr | awk -F\\' \'{ print $2 }\'| ";
 while (<READ>) {
   if ($_ =~ /qbr/) {
-		@split = split /'/,$_;
-		my $bridge = $split[1];
-		$usedbridgelist{$bridge}++;
+    @split = split /'/,$_;
+    my $bridge = $split[1];
+    $usedbridgelist{$bridge}++;
+    print $bridge . "\n" if ($debug);
   }
 }
-open READ, "brctl show |";
+#cat /etc/libvirt/qemu/*.xml | grep "qbr" | awk -F \' '{ print $2 }'
+open READ, "brctl show | ";
 while (<READ>) {
-	chop $_;
-	if ($_ =~ /qbr/) {
-		my @split = split /\t/, $_;
-		$presentbridge{$split[0]}=$split[5];
-	}
+  chop $_;
+  if ($_ =~ /qbr/) {
+    my @split = split /\t/, $_;
+    print $split[0], " " , $split[5], "\n" if ($debug);
+    $presentbridge{$split[0]}=$split[5];
+  }
 }
 foreach my $bridge (sort keys %presentbridge) {
-	if ($usedbridgelist{$bridge}) {
-		print "keeping $bridge\n";
-	} else {
-		`ifconfig $bridge down`;
-		`brctl delif $bridge $presentbridge{$bridge}`;
-		`brctl delbr $bridge`;
-		print "Deleting $bridge and $presentbridge{$bridge}\n";
-		my $ovsbridge=$bridge;
-		$ovsbridge=~s/qbr/qvo/g;
-		`ovs-vsctl del-port $ovsbridge`;
-	}
+  if ($usedbridgelist{$bridge}) {
+    print "keeping $bridge\n";
+  } else {
+    `ifconfig $bridge down`;
+    `brctl delif $bridge $presentbridge{$bridge}`;
+    `brctl delbr $bridge`;
+    print "Deleting $bridge and $presentbridge{$bridge}\n";
+    my $ovsbridge=$bridge;
+    $ovsbridge=~s/qbr/qvo/g;
+    `ovs-vsctl del-port $ovsbridge`;
+  }
 }
