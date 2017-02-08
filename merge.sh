33e601bb773e5a2b8f54914b4ea28171421e6777
Dave Hill <dhill@redhat.com>
2017-02-08 14:29:19 -0500

    Fixing images get.

--- merge.sh
+++ merge.sh
@@ -1,4 +1,5 @@
 commit=$(git log | grep commit | head -1 | awk '{ print $2 }' )
+commit=2900d89303a6bf577d73e5ad52c8c655d2f8c5a9
 
 for p in $(git branch  | awk '{ print $1 }' | grep -v '\*'); do
   echo $p
