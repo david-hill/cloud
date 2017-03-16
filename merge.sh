bcc579e44f0c252d7286d351cd16328ecffac2b5
Dave Hill <dhill@redhat.com>
2017-03-16 19:05:51 -0400

    Fixing virt-customize.  Editing the debug line instead of virt-customize wasn't the proper solution to this issue.

--- merge.sh
+++ merge.sh
@@ -1,4 +1,5 @@
 commit=$(git log | grep commit | head -1 | awk '{ print $2 }' )
+commit=007c1bfc406a452980e633162201f2668339bf7c
 
 for p in $(git branch  | awk '{ print $1 }' | grep -v '\*'); do
   echo $p
