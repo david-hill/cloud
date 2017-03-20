7a6324f3080bcb03648fba72da4b7ea23550d37a
Dave Hill <dhill@redhat.com>
2017-03-20 12:02:39 -0400

    Verbose in create_undercloud.sh

--- create_undercloud.sh
+++ create_undercloud.sh
@@ -175,12 +175,14 @@ if [ $rc -eq 0 ]; then
           if [ ! -z $rdorelease ]; then
             wait_for_reboot
             if [ $? -eq 0 ]; then
-              startlog "Uploading RHEL image"
-              ssh -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stack@$undercloudip 'if [ ! -e images ]; then mkdir images; fi' > /dev/null
+              startlog "Customizing RHEL image"
               rhelimage=$(ls -atr images/rhel/ | grep qcow2 | grep $rhel | tail -1)
               sudo cp images/rhel/$rhelimage rhel-guest-image-local.qcow2
               sudo chown qemu rhel-guest-image-local.qcow2
               sudo virt-customize -v -a rhel-guest-image-local.qcow2 $uploadcmd iptables:/etc/sysconfig/ 2>>$stderr 1>>$stdout
+              endlog "done"
+              startlog "Uploading RHEL image"
+              ssh -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stack@$undercloudip 'if [ ! -e images ]; then mkdir images; fi' > /dev/null
               scp -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no rhel-guest-image-local.qcow2 stack@$undercloudip:images/ 2>>$stderr 1>>$stdout
               endlog "done"
               cd images
