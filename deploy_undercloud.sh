d64da14d67cc839f7002ee7ffba6a75087192520
Dave Hill <dhill@redhat.com>
2017-08-05 17:12:57 -0400

    Removing the test in deploy_undercloud.sh

--- deploy_undercloud.sh
+++ deploy_undercloud.sh
@@ -116,7 +116,6 @@ function baremetal_setup {
     rc=$?
     if [ $rc -eq 0 ]; then
       endlog "done"
-      sleep 60
       startlog "Starting introspection"
       openstack baremetal introspection bulk start 2>>$stderr 1>>$stdout
       rc=$?
