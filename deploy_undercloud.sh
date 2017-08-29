b561ab7d98ce9e70aa9ec393b95e8e7d92497221
Dave Hill <dhill@redhat.com>
2017-08-29 09:12:14 -0400

    Fixing typo in deploy_undercloud.sh

--- deploy_undercloud.sh
+++ deploy_undercloud.sh
@@ -257,7 +257,7 @@ if [ $rc -eq 0 ]; then
     if [ $rc -eq 0 ]; then 
       test_overcloud
       rc=$?
-      if [ $? -eq 0 ]; then
+      if [ $rc -eq 0 ]; then
         touch /home/stack/deployment_state/tested
       fi
     fi
