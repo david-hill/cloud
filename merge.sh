424c1fd3bdea1c50559197cabfcbe0cfa51929b1
Dave Hill <dhill@redhat.com>
2017-09-13 15:12:22 -0400

    Fixing merge.sh

--- merge.sh
+++ merge.sh
@@ -1,5 +1,4 @@
 commit=$(git log | grep commit | head -1 | awk '{ print $2 }' )
-commit=007c1bfc406a452980e633162201f2668339bf7c
 
 for p in $(git branch  | awk '{ print $1 }' | grep -v '\*'); do
   echo $p
