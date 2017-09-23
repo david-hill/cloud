b9652d85093d6e33588edd6fee97464ee103f102
Dave Hill <dhill@redhat.com>
2017-09-23 17:15:45 -0400

    Removing the sleep 600.

--- deploy_undercloud.sh
+++ deploy_undercloud.sh
@@ -156,7 +156,6 @@ function baremetal_setup {
       if [ $rc -eq 0 ]; then
         endlog "done"
         clear_arp_table
-        sleep 600
         if [ ! -d "/home/stack/deployment_state" ]; then
           mkdir -p /home/stack/deployment_state
         fi
