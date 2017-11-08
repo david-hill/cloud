f615ab39d4c1f156d5d864163dcc2b65c213405c
Dave Hill <dhill@redhat.com>
2017-11-07 20:43:18 -0500

    Make sure the VMs are not stuck in "in shutdown" in stop_vms.sh

--- stop_vms.sh
+++ stop_vms.sh
@@ -2,5 +2,18 @@ source functions
 source_rc setup.cfg
 for p in $(sudo virsh list | grep running | awk '{ print $1 }'); do 
   sudo virsh destroy $p 2>>$stderr 1>>$stdout
+  rc=$?
 done
-exit 0
+
+timeout=60
+while [ $timeout -ne 0 ] && [ $rc -ne 0 ]; do
+  sudo virsh list | grep -q "in shutdown"
+  if [ $? -eq 0 ]; then
+    rc=255
+  else
+    rc=0
+  fi
+  sleep 1
+  timeout=$(( $timeout - 1 ))
+done
+exit $rc
