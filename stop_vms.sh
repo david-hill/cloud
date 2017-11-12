e8ca709b58d08f5f91ebf237ea58507caa6de18b
Dave Hill <dhill@redhat.com>
2017-11-12 16:03:45 -0500

    Wait 60s for VM stuck "in shutdown" and then bail out.

--- stop_vms.sh
+++ stop_vms.sh
@@ -5,15 +5,18 @@ for p in $(sudo virsh list | grep running | awk '{ print $1 }'); do
   rc=$?
 done
 
-timeout=60
-while [ $timeout -ne 0 ] && [ $rc -ne 0 ]; do
-  sudo virsh list | grep -q "in shutdown"
-  if [ $? -eq 0 ]; then
-    rc=255
-  else
-    rc=0
-  fi
-  sleep 1
-  timeout=$(( $timeout - 1 ))
-done
+if [ $rc -eq 0 ]; then
+  rc=255
+  timeout=60
+  while [ $timeout -ne 0 ] && [ $rc -ne 0 ]; do
+    sudo virsh list | grep -q "in shutdown"
+    if [ $? -eq 0 ]; then
+      rc=255
+    else
+      rc=0
+    fi
+    sleep 1
+    timeout=$(( $timeout - 1 ))
+  done
+fi
 exit $rc
