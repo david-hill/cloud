dce22540135de58f614c840e0c2bf1d79638cb9a
Dave Hill <dhill@redhat.com>
2018-01-31 10:07:57 -0500

    Use nested virtualisation if it's enabled.

--- merge.sh
+++ merge.sh
@@ -1,4 +1,5 @@
-commit=$(git log | grep commit | head -1 | awk '{ print $2 }' )
+#commit=$(git log | grep commit | head -1 | awk '{ print $2 }' )
+commit=ab15f375fd5bc37d6bfccc9b13b6db8bd36d145c
 
 for p in $(git branch  | awk '{ print $1 }' | grep -v '\*'); do
   echo $p
