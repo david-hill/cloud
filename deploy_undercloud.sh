03f260ca909da940628bc4b99665d1a522a2e8b3
Dave Hill <dhill@redhat.com>
2018-03-03 11:08:19 -0500

    Removing a useless "if/else" clause.

--- deploy_undercloud.sh
+++ deploy_undercloud.sh
@@ -391,26 +391,22 @@ function create_local_docker_registry {
       if [ -z "$tag" ]; then
         tag=latest
       fi
-      if [ ! -z $tag ]; then
+      endlog "done"
+      startlog "Preparing local image registry"
+      openstack overcloud container image prepare --namespace=${url}/${releasever} --prefix=openstack- --tag=$tag --output-images-file /home/stack/${releasever}/local_registry_images.yaml 2>>$stderr 1>>$stdout
+      rc=$?
+      if [ $rc -eq 0 ]; then
         endlog "done"
-        startlog "Preparing local image registry"
-        openstack overcloud container image prepare --namespace=${url}/${releasever} --prefix=openstack- --tag=$tag --output-images-file /home/stack/${releasever}/local_registry_images.yaml 2>>$stderr 1>>$stdout
+        startlog "Uploading local image registry"
+        sudo openstack overcloud container image upload --config-file  /home/stack/${releasever}/local_registry_images.yaml --verbose 2>>$stderr 1>>$stdout
         rc=$?
         if [ $rc -eq 0 ]; then
           endlog "done"
-          startlog "Uploading local image registry"
-          sudo openstack overcloud container image upload --config-file  /home/stack/${releasever}/local_registry_images.yaml --verbose 2>>$stderr 1>>$stdout
+          startlog "Preparing local container image registry"
+          openstack overcloud container image prepare --namespace=192.0.2.1:8787/${releasever} --prefix=openstack- --tag=$tag --output-env-file=/home/stack/${releasever}/overcloud_images.yaml 2>>$stderr 1>>$stdout
           rc=$?
           if [ $rc -eq 0 ]; then
             endlog "done"
-            startlog "Preparing local container image registry"
-            openstack overcloud container image prepare --namespace=192.0.2.1:8787/${releasever} --prefix=openstack- --tag=$tag --output-env-file=/home/stack/${releasever}/overcloud_images.yaml 2>>$stderr 1>>$stdout
-            rc=$?
-            if [ $rc -eq 0 ]; then
-              endlog "done"
-            else
-              endlog "error"
-            fi
           else
             endlog "error"
           fi
