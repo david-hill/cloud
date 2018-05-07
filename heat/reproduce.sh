1626f607547f71c13b2c32903fdc23d832915c4e
root <root@undercloud-7.default.redhat.local>
2018-05-07 12:22:04 -0400

    Adding reproduce.sh

---
+++ heat/reproduce.sh
@@ -0,0 +1,41 @@
+function do_ping {
+  inc=0
+  rc=1
+  while [ $inc -lt 10 ] && [ $rc -ne 0 ]; do
+    echo "ping $ip - $inc"
+    ping -c1 $ip
+    rc=$?
+    inc=$(( $inc + 1 ))
+  done
+  return $rc
+}
+
+
+heat stack-create -f stack_5router-ext.yaml test
+if [ $? -eq 0 ]; then
+
+  completed=1
+  
+  while [ $completed -eq 1 ]; do
+    heat stack-list | grep COMPLETE
+    completed=$?
+  done
+  
+  neutron port-list > after
+  
+  ip=$(diff before after | grep 10.0.0 | awk -F\| '{ print $5 }' | awk -F\" '{ print $8 }' )
+  
+  
+  do_ping $ip
+  if [ $? -eq 0 ]; then
+    echo worked
+  
+    heat stack-delete -y test
+    
+    completed=0
+    while [ $completed -eq 0 ]; do
+      heat stack-list | grep test
+      completed=$?
+    done
+  fi
+fi
