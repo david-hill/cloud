633ce3a523caea6f2c925942aeb2d77a2eac76ae
Dave Hill <dhill@redhat.com>
2018-06-04 15:41:29 -0400

    Fixing error checking for second disk in create_virsh_vms.sh

--- create_virsh_vms.sh
+++ create_virsh_vms.sh
@@ -32,11 +32,13 @@ function gen_disks {
   if [ $rc -eq 0 ]; then
     restore_permissions $tpath/$type-$inc-$releasever.qcow2
     rc=$?
-    sudo qemu-img create -f qcow2 $tpath/$type-$inc-$releasever-vdb.qcow2 50G > /dev/null
-    rc=$?
     if [ $rc -eq 0 ]; then
-      restore_permissions $tpath/$type-$inc-$releasever-vdb.qcow2
+      sudo qemu-img create -f qcow2 $tpath/$type-$inc-$releasever-vdb.qcow2 50G > /dev/null
       rc=$?
+      if [ $rc -eq 0 ]; then
+        restore_permissions $tpath/$type-$inc-$releasever-vdb.qcow2
+        rc=$?
+      fi
     fi
   fi
   return $rc
