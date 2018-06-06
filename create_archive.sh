73e11ad6bde9fa91e7c45adbd7f7037cca1397cf
Dave Hill <dhill@redhat.com>
2018-06-06 17:19:32 -0400

    Validate we're running networkx 1.9 or earlier.

--- create_archive.sh
+++ create_archive.sh
@@ -3,10 +3,11 @@ tmpfile=$(mktemp)
 tmpfolder=$(mktemp -d)
 
 url=$1
-url=http://download-node-02.eng.bos.redhat.com/brewroot/packages/openstack-nova/12.0.6/22.el7ost/data/signed/f21541eb/noarch/
+#url=http://download-node-02.eng.bos.redhat.com/brewroot/packages/dbus/1.10.22/1.el7/x86_64/
+url=http://download-node-02.eng.bos.redhat.com/brewroot/packages/openstack-neutron-lbaas/9.2.2/7.el7/noarch/
 curl -s $url > $tmpfile
 
-for p in $(cat $tmpfile | grep rpm | awk -F\" '{ print $6 }'); do
+for p in $(cat $tmpfile | grep rpm | awk -F\" '{ print $6 }' | grep -v "\.src"); do
   wget $url/$p -O $tmpfolder/$p
 done
 
