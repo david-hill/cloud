0f5a60cd56b4089adf2ac6e6e27cf788111a4a17
Dave Hill <dhill@redhat.com>
2018-06-27 08:18:49 -0400

    Adding a comment in cleanup-docker-remote-registry.sh

--- ops/cleanup-docker-remote-registry.sh
+++ ops/cleanup-docker-remote-registry.sh
@@ -2,6 +2,9 @@ source /home/stack/stackrc
 
 deletetags="12.0-20180529.1"
 
+# mmethot prefers this one :
+# nova list | awk '/ACTIVE/ {gsub(/ctlplane=/, ""); print $12}'
+
 for tag in $deletetags; do
   for id in $( docker images | awk '{ print $3 }' | sort | uniq ); do
     for p in $(nova list | grep ACTIVE | awk -F= '{ print $2 }' | awk '{ print $1 }'); do
