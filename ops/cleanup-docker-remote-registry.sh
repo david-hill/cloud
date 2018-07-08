0cb769567d9b03369c684656769698abb8cb0c1e
Dave Hill <dhill@redhat.com>
2018-07-08 15:31:42 -0400

    Fixing cleanup-docker-remote-registry.sh

--- ops/cleanup-docker-remote-registry.sh
+++ ops/cleanup-docker-remote-registry.sh
@@ -6,7 +6,7 @@ deletetags="12.0-20180529.1"
 # nova list | awk '/ACTIVE/ {gsub(/ctlplane=/, ""); print $12}'
 
 for tag in $deletetags; do
-  for id in $( docker images | awk '{ print $3 }' | sort | uniq ); do
+  for id in $( docker images | grep $tag | awk '{ print $3 }' | sort | uniq ); do
     for p in $(nova list | grep ACTIVE | awk -F= '{ print $2 }' | awk '{ print $1 }'); do
       ssh heat-admin@$p "sudo docker rmi -f $id" 
     done
