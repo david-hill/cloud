f6bdfb0d2acdc223a527e8aad920f8ca7e82d418
Dave Hill <dhill@redhat.com>
2018-08-22 21:39:00 -0400

    Improving resume_vbmc.sh in order to have some output.

--- resume_vbmc.sh
+++ resume_vbmc.sh
@@ -1,3 +1,10 @@
-vbmc list | grep rhosp | grep down | awk '{ print $6 }' | xargs -I{} ip a add {}/32 dev virbr0
-vbmc list | grep rhosp | grep down | awk '{ print $2 }' | xargs -I{} vbmc start {}
+for p in $( vbmc list | grep rhosp | grep down | awk '{ print $6 }' ); do
+  echo Add $p to virbr0
+  ip a add $p/32 dev virbr0
+done
+
+for p in $( vbmc list | grep rhosp | grep down | awk '{ print $2 }' ); do
+  echo Start $p
+  vbmc start $p
+done
 
