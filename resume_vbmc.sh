aba2796ed51bf23691cc1e020d65742fae0928ea
Dave Hill <dhill@redhat.com>
2018-08-23 08:44:59 -0400

    Workarounds for slow vbmc stop and failures to properly start vbmc ...

--- resume_vbmc.sh
+++ resume_vbmc.sh
@@ -6,11 +6,22 @@ for p in $( vbmc list | grep rhosp | grep down | awk '{ print $6 }' ); do
   fi
 done
 
-for p in $( vbmc list | grep rhosp | grep down | awk '{ print $2 }' ); do
+function start_vbmc {
+  p=$1
   vbmc show $p | grep -q "status.*down"
   if [ $? -eq 0 ]; then
     echo Start $p
     vbmc start $p
+    vbmc show $p | grep -q "status.*down"
+    if [ $? -eq 0 ]; then
+      sleep 1
+      start_vbmc $p
+    fi 
   fi
+}
+
+
+for p in $( vbmc list | grep rhosp | grep down | awk '{ print $2 }' ); do
+  start_vbmc $p
 done
 
