565d4290dbc1e57e9c7da8f30f862c3ec0e8b0ac
Dave Hill <dhill@redhat.com>
2018-10-02 17:10:01 -0400

    Prevent a fork bomb in resume_vbmc.sh .

--- resume_vbmc.sh
+++ resume_vbmc.sh
@@ -8,20 +8,24 @@ done
 
 function start_vbmc {
   p=$1
-  vbmc show $p | grep -q "status.*down"
-  if [ $? -eq 0 ]; then
-    echo Start $p
-    vbmc start $p
+  inc=$2
+  inc=$(( $inc + 1 ))
+  if [ $inc -lt 3 ]; then
     vbmc show $p | grep -q "status.*down"
     if [ $? -eq 0 ]; then
-      sleep 1
-      start_vbmc $p
-    fi 
+      echo Start $p
+      vbmc start $p
+      vbmc show $p | grep -q "status.*down"
+      if [ $? -eq 0 ]; then
+        sleep 1
+        start_vbmc $p $inc
+      fi 
+    fi
   fi
 }
 
 
 for p in $( vbmc list | grep rhosp | grep down | awk '{ print $2 }' ); do
-  start_vbmc $p
+  start_vbmc $p 0
 done
 
