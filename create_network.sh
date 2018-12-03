9dbfbd1e7a37ff40f9a982a8d565aef0d6b04d76
Dave Hill <dhill@redhat.com>
2018-12-03 09:34:01 -0500

    Fix create_network.sh

--- create_network.sh
+++ create_network.sh
@@ -5,6 +5,18 @@ source_rc setup.cfg
 source_rc overcloudrc
 
 
+function add_router_interface {
+  startlog "Adding interface to router"
+  neutron router-interface-add test-router test-subnet 2>>$stderr 1>>$stdout
+  rc=$?
+  if [ $rc -eq 0 ]; then
+    endlog "done"
+  else
+    endlog "error"
+    rc=255
+  fi
+  return $rc
+}
 
 function create_router {
     startlog "Creating router"
@@ -42,6 +54,44 @@ function create_ext_subnet {
 	return $rc
 }
 
+function set_external_gateway {
+    startlog "Setting external gateway"
+n router-port-list test-router | grep 10.254.0neutron router-port-list test-router | grep 10.254.0
+    neutron router-gateway-set test-router ext-net 2>>$stderr 1>>$stdout
+    rc=$?
+    if [ $rc -eq 0 ]; then
+      endlog "done"
+    else
+      endlog "error"
+      rc=255
+    fi
+  return $rc
+}
+function create_test_network {
+  startlog "Creating test network"
+  neutron net-create --provider:network_type $neutronnwtype test 2>>$stderr 1>>$stdout
+  rc=$?
+  if [ $rc -eq 0 ]; then
+    endlog "done"
+  else
+    endlog "error"
+    rc=255
+  fi
+  return $rc
+}
+
+function create_test_subnet {
+  startlog "Creating test subnet"
+  neutron subnet-create test 10.254.0.0/16 --name test-subnet 2>>$stderr 1>>$stdout
+  rc=$?
+  if [ $rc -eq 0 ]; then
+    endlog "done"
+  else
+    endlog "error"
+    rc=255
+  fi
+  return $rc
+}
 if [ $? -eq 0 ]; then
   if [ ! -z "$gw" ]; then
     gwarg="--gateway $gw"
@@ -50,60 +100,59 @@ if [ $? -eq 0 ]; then
   fi
 
   rc=0
+
   neutron router-list 2>>$stderr 2>>$stderr | grep -q test-router
   if [ $? -ne 0 ]; then
     create_router
     rc=$?
   fi
+
   if [ $rc -eq 0 ]; then
     neutron net-list 2>>$stderr | grep -q ext-net
     if [ $? -ne 0 ]; then
        create_ext_network
        rc=$?
     fi
-    if [ $rc -eq 0 ]; then
-      neutron subnet-list 2>>$stderr | grep -q ext-subnet
-      if [ $? -ne 0 ]; then
-         create_ext_subnet
-         rc=$?
-      fi
+  fi
+
+  if [ $rc -eq 0 ]; then
+    neutron subnet-list 2>>$stderr | grep -q ext-subnet
+    if [ $? -ne 0 ]; then
+       create_ext_subnet
+       rc=$?
     fi
   fi
-  startlog "Setting external gateway"
-  neutron router-gateway-set test-router ext-net 2>>$stderr 1>>$stdout
-  rc=$?
+
   if [ $rc -eq 0 ]; then
-    endlog "done"
-    startlog "Creating test network"
-    neutron net-create --provider:network_type $neutronnwtype test 2>>$stderr 1>>$stdout
-    rc=$?
-    if [ $rc -eq 0 ]; then
-      endlog "done"
-      startlog "Creating test subnet"
-      neutron subnet-create test 10.254.0.0/16 --name test-subnet 2>>$stderr 1>>$stdout
+    neutron router-show test-router | grep gateway | grep -q ip_address
+    if [ $? -ne 0 ] then
+      set_external_gateway
       rc=$?
-      if [ $rc -eq 0 ]; then
-        endlog "done"
-        startlog "Adding interface to router"
-        neutron router-interface-add test-router test-subnet 2>>$stderr 1>>$stdout
-        rc=$?
-        if [ $rc -eq 0 ]; then
-          endlog "done"
-        else
-          endlog "error"
-          rc=255
-        fi
-      else
-        endlog "error"
-        rc=255
-      fi
-    else
-      endlog "error"
-      rc=255
     fi
-  else
-    endlog "error"
-    rc=255
+  fi
+
+  if [ $rc -eq 0 ]; then
+    neutron net-list | grep -q test
+    if [ $? -ne 0 ]; then
+      create_test_network
+      rc=$?
+    fi
+  fi
+
+  if [ $rc -eq 0 ]; then
+    neutron subnet-list | grep -q test-subnet
+    if [ $? -eq 0 ]; then
+      create_test_subnet
+      rc=$?
+    fi
+  fi
+
+  if [ $rc -eq 0 ]; then
+    neutron router-port-list test-router | grep -q10.254.
+    if [ $? -ne 0 ]; then
+      add_router_interface
+      rc=$?
+    fi
   fi
 fi
 exit $rc
