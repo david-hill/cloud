78c5a4235904d96b40349bd177e64a46f9c3369f
David Hill <dhill@redhat.com>
2019-02-07 11:29:43 -0500

    Adding some checks and creating /root/db

--- ops/backup_db.sh
+++ ops/backup_db.sh
@@ -1,4 +1,11 @@
 DATE=`date '+%Y%m%d%H%M%S'`
 
-mysqldump --all-database | gzip > /root/db/backup.$DATE.sql.gz
 
+if [ ! -d /root/db ]; then
+  mkdir /root/db
+fi
+
+mysqldump --all-databases | gzip > /root/db/backup.$DATE.sql.gz
+if [ $? -ne 0 ]; then
+  echo Warning!
+fi
