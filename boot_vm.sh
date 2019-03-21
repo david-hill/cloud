aeddec619c48d825f795259f3606507423514398
David Hill <dhill@redhat.com>
2019-03-21 09:42:59 -0400

    Create a boot from volume instance in boot_vm.sh

--- boot_vm.sh
+++ boot_vm.sh
@@ -98,6 +98,10 @@ function unprovision_vm {
       if [ $rc -eq 0 ]; then
         delete_keypair
         rc=$?
+        if [ $rc -eq 0 ]; then
+          delete_volume
+          rc=$?
+        fi
       fi
     fi
   fi
@@ -136,6 +140,17 @@ function create_volume {
   fi
 }
 
+function delete_volume {
+  startlog "Deleting volume $volid"
+  cinder delete $volid 2>>$stderr 1>>$stdout
+  rc=$?
+  if [ $rc -eq 0 ]; then
+    endlog "done"
+  else
+    endlog "error"
+  fi
+  return $rc
+}
 function wait_for_volume {
   inc=0
   while ! $( cinder list | grep $volid | egrep -q "available|error") && [ $inc -lt 10 ]; do
@@ -148,13 +163,13 @@ function create_boot_from_volume_test_vm {
   nova list 2>>$stderr | grep -q test-vm
   rc=$?
   if [ $rc -eq 1 ]; then
-    startlog "Creating test VM"
     create_volume
-
+    startlog "Creating test VM"
     if [ ! -z $volid ]; then
       wait_for_volume
       if $( cinder list | grep $volid | grep -q available ); then
-        nova boot --flavor m1.micro --block-device source=volume,id=$volid,dest=volume,size=1,shutdown=preserve,bootindex=0  --nic net-id=$neutron test-vm 2>>$stderr 1>>$stdout
+#        nova boot --flavor m1.micro --block-device source=volume,id=$volid,dest=volume,size=1,shutdown=preserve,bootindex=0  --nic net-id=$neutron test-vm 2>>$stderr 1>>$stdout
+        nova boot --flavor m1.micro --block-device source=volume,id=$volid,dest=volume,size=1,shutdown=remove,bootindex=0  --nic net-id=$neutron test-vm 2>>$stderr 1>>$stdout
         rc=$?
       else
         rc=252
@@ -390,7 +405,8 @@ function provision_vm {
     create_keypair
     rc=$?
     if [ $rc -eq 0 ]; then
-      create_test_vm
+#      create_test_vm
+      create_boot_from_volume_test_vm
       rc=$?
       if [ $rc -eq 0 ]; then
         wait_for_vm
