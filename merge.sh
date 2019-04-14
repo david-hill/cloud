52c71b49eb8f76b080ee25997430849b3326a93d
David Hill <dhill@redhat.com>
2019-04-14 14:13:41 -0400

    Merging latest changes in merge.sh

--- merge.sh
+++ merge.sh
@@ -1,10 +1,16 @@
-#commit=$(git log | grep commit | head -1 | awk '{ print $2 }' )
-commit=ab15f375fd5bc37d6bfccc9b13b6db8bd36d145c
+commits=$(git log | grep commit | head -1 | awk '{ print $2 }' )
+
 
 for p in $(git branch  | awk '{ print $1 }' | grep -v '\*'); do
-  echo $p
   git checkout $p
   git pull
-  git cherry-pick $commit
-  git push
+  for commit in $commits; do
+    git cherry-pick $commit
+    if [ $? -ne 0 ]; then
+      git reset --hard
+      git push
+    else
+      git push
+    fi
+  done
 done
