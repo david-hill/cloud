4da6748387f5e52e9be0b4c576f84b9b4c6f0809
David Hill <dhill@redhat.com>
2019-09-17 13:06:25 -0400

    Improved cleanup_swift.sh

--- ops/cleanup_swift.sh
+++ ops/cleanup_swift.sh
@@ -1,22 +1,35 @@
 source gnocchirc
 
-ls=10
+function delete_objects {
+  container=$1
+  ls=10
+  while [ $ls -gt 0 ]; do
+    timeout 300 swift list $container > output
+    split -l1000 output
+    ls=$( ls x* | wc -l )
+  
+    for p in x*; do
+       echo $p
+       time  swift delete $container $(cat $p ) > /dev/null
+       rm -rf $p
+    done
+  done
+ } 
+  
+function delete_containers {
+  for p in $(swift list ); do 
+    echo $p
+    time swift delete $p > /dev/null
+  done
+}
 
-while [ $ls -gt 0 ]; do
-   timeout 300 swift list measure > output
-   split -l1000 output
-   ls=$( ls x* | wc -l )
+function count_containers {
+  var=$( swift list | wc -l )
+}
 
-   for p in x*; do
-      echo $p
-      time  swift delete measure $(cat $p ) > /dev/null
-      rm -rf $p
-   done
+delete_objects measure
+count_containers
+while [ $var -gt 0 ]; do
+  delete_containers
+  count_containers
 done
-
-
-for p in $(swift list | grep gnocchi ); do 
-  echo $p
-  time swift delete $p > /dev/null
-done
-
