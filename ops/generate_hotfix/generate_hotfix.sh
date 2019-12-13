2d1254889fd72a0fff355ae0ae8b9ad7fe9e09a2
David Hill <dhill@redhat.com>
2019-12-12 19:31:36 -0500

    Adding a warning message if /home/stack/templates/overcloud_images.yaml is not found.

--- ops/generate_hotfix/generate_hotfix.sh
+++ ops/generate_hotfix/generate_hotfix.sh
@@ -29,6 +29,10 @@ ln -s $PWD/ansible-role-tripleo-modify-image $PWD/ansible-role-tripleo-modify-im
 
 registry=registry.access.redhat.com
 
+if [ ! -e /home/stack/templates/overcloud_images.yaml ]; then
+  echo WARNING: /home/stack/templates/overcloud_images.yaml was not found .  You will have to manually edit the file to add the generated image_tag to your overcloud_images.yaml file.
+fi
+
 if [ -e /home/stack/local_registry_images.yaml ]; then
   registry=$( cat /home/stack/local_registry_images.yaml  | grep imagename |  awk '{ print $3 }' | cut -d\/ -f1 | tail -1 )
 fi
