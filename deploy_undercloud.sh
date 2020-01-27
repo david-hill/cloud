1b8cbcf118d341fd56140c95b26596375fd6b7dc
Dave Hill <dhill@redhat.com>
2020-01-27 16:58:53 -0500

    brewweb is replaced by the new registry.

--- deploy_undercloud.sh
+++ deploy_undercloud.sh
@@ -513,7 +513,8 @@ function get_docker_url {
       if [ -z $dockerregistry ]; then
 	      if [ "$alpha" -eq 1 ]; then
           #url=registry.redhat.io/rhosp-beta
-          url=brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888
+          url=registry-proxy.engineering.redhat.com/rh-osbs
+          #url=brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888
         else
           url=docker-registry.engineering.redhat.com
         fi
@@ -581,6 +582,8 @@ EOF
               sed -i -e "s/rhceph-4.0-rhel8/rhceph-4-rhel8/g" /home/stack/containers-prepare-parameter.yaml
               rc=$?
               sed -i -e "s/ceph_tag: .*/ceph_tag: 4-8/g" /home/stack/containers-prepare-parameter.yaml
+              rc=$?
+              sed -i -e "s/name_prefix: .*/name_prefix: rhosp$vernum-openstack-/g" /home/stack/containers-prepare-parameter.yaml
               rc=$?
             fi
           fi
