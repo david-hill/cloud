520f26738b2e7a45ffcf8262ffe9e78a2fe55e53
David Hill <dhill@redhat.com>
2020-07-15 10:20:38 -0400

    Adding neutron_driver default value to setup.cfg.

--- create_archive.sh
+++ create_archive.sh
@@ -4,10 +4,10 @@ tmpfolder=$(mktemp -d)
 
 url=$1
 #url=http://download-node-02.eng.bos.redhat.com/brewroot/packages/dbus/1.10.22/1.el7/x86_64/
-url=http://download-node-02.eng.bos.redhat.com/brewroot/packages/openstack-neutron-lbaas/9.2.2/7.el7/noarch/
+url=http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libvirt/6.4.0/1.scrmod+el8.3.0+7066+6dd3ecaa/x86_64/
 curl -s $url > $tmpfile
 
-for p in $(cat $tmpfile | grep rpm | awk -F\" '{ print $6 }' | grep -v "\.src"); do
+for p in $(cat $tmpfile | grep rpm | awk -F\" '{ print $6 }' | grep -v debuginfo | grep -v "\.src"); do
   wget $url/$p -O $tmpfolder/$p
 done
 
