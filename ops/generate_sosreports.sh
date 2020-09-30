03752a9ce9b6050594a44bf7ef2e29516b477f79
David Hill <dhill@redhat.com>
2020-09-30 14:17:43 -0400

    Remove "-s bash -c" in the ssh command for sosreport to work properly.

--- ops/generate_sosreports.sh
+++ ops/generate_sosreports.sh
@@ -2,6 +2,6 @@ source stackrc
 args="--all-logs"
 for p in $(openstack server list -c Networks -f value | sed -e 's/ctlplane=//g'); do 
   echo $p
-  ssh heat-admin@$p "sudo -s bash -c sosreport --batch $args"
+  ssh heat-admin@$p "sudo sosreport --batch $args"
 done
 
