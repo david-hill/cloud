64691c6b2660906d2f3d105bbbd5a3d797817e2d
Dave Hill <dhill@redhat.com>
2022-01-28 11:56:39 -0500

    Added raid_level:1 in tag_hosts.sh

--- ops/soft_raid/tag_hosts.sh
+++ ops/soft_raid/tag_hosts.sh
@@ -2,9 +2,9 @@ nodes="control-0-rhosp16 control-1-rhosp16 control-2-rhosp16 compute-0-rhosp16 c
 boot_mode=bios
 for p in $nodes; do
   if [[ $p =~ control ]]; then
-    openstack baremetal node set --property capabilities="profile:control,boot_mode:${boot_mode},boot_option:local" $p
+    openstack baremetal node set --property capabilities="profile:control,boot_mode:${boot_mode},boot_option:local,raid_level:1" $p
   else
-    openstack baremetal node set --property capabilities="profile:compute,boot_mode:${boot_mode},boot_option:local" $p
+    openstack baremetal node set --property capabilities="profile:compute,boot_mode:${boot_mode},boot_option:local,raid_level:1" $p
     fi
     done
 
