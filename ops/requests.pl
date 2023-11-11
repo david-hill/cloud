1c933e36b4753a3f2dcd1752570c28b56e9ed758
David Hill <dhill@redhat.com>
2023-11-11 13:35:13 -0500

    Updated ops/requests.pl to use available libraries :(

--- ops/requests.pl
+++ ops/requests.pl
@@ -1,17 +1,25 @@
-use Date::Parse;
+#use Date::Parse;
+use Time::Local;
 
+`zcat */var/log/containers/heat/heat-engine*gz > req`;
+`cat */var/log/containers/heat/heat-engine.log */var/log/containers/heat/heat-engine.log.1 >> req`;
 
 open READ, "req";
 while (<READ>) {
-        if ($_ =~ /^([0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+\.[0-9]+)/) {
-                $date=$1;
+        if ($_ =~ /^([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+):([0-9]+)\.[0-9]+/) {
+                $y = $1;
+                $m = $2;
+                $d = $3;
+                $h = $4;
+                $M = $5;
+                $s = $6;
         }
         if ($_ =~ /(req-[a-z0-9-]+)/) {
                 $treq=$1;
                 if (! $req{$treq}{'first-seen'} ){
-                    $req{$treq}{'first-seen'}=str2time($date);
+                    $req{$treq}{'first-seen'}=timelocal($s,$M,$h, $d, $m, $y);
             } else {
-                    $req{$treq}{'last-seen'}=str2time($date);
+                    $req{$treq}{'last-seen'}=timelocal($s, $M, $h, $d, $m, $y);
                     $req{$treq}{'elapsed'}=$req{$treq}{'last-seen'} - $req{$treq}{'first-seen'};
                     }
         }
