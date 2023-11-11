b8c7b1b5134856bb27a53fa41dffe17841547123
David Hill <dhill@redhat.com>
2023-11-11 14:34:52 -0500

    Some improvements to ops/requests.pl

--- ops/requests.pl
+++ ops/requests.pl
@@ -1,9 +1,15 @@
 #use Date::Parse;
 use Time::Local;
+sub pad() {
+        $value = shift;
+        if ($value < 10) {
+                $value = "0$value";
+        }
+        return $value;
+}
 
 `zcat */var/log/containers/heat/heat-engine*gz > req`;
 `cat */var/log/containers/heat/heat-engine.log */var/log/containers/heat/heat-engine.log.1 >> req`;
-
 open READ, "req";
 while (<READ>) {
         if ($_ =~ /^([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+):([0-9]+)\.[0-9]+/) {
@@ -15,17 +21,28 @@ while (<READ>) {
                 $s = $6;
         }
         if ($_ =~ /(req-[a-z0-9-]+)/) {
-                $treq=$1;
-                if (! $req{$treq}{'first-seen'} ){
+             $treq=$1;
+             if (! $req{$treq}{'first-seen'} ){
                     $req{$treq}{'first-seen'}=timelocal($s,$M,$h, $d, $m, $y);
+                    $req{$treq}{'last-seen'}=$req{$treq}{'first-seen'};
+                    $req{$treq}{'elapsed'}=0;
             } else {
                     $req{$treq}{'last-seen'}=timelocal($s, $M, $h, $d, $m, $y);
                     $req{$treq}{'elapsed'}=$req{$treq}{'last-seen'} - $req{$treq}{'first-seen'};
-                    }
+            }
         }
 }
-
 foreach $req (sort keys %req) {
-        print $req ."\t" .$req{$req}{'elapsed'} . "\n";
+        my ($sec, $min, $hour, $day,$month,$year) = (localtime($req{$req}{'last-seen'}))[0,1,2,3,4,5];
+        $year += 1900;
+        $month += 1;
+        $month = &pad($month);
+        $day = &pad($day);
+        $hour = &pad($hour);
+        $min = &pad($min);
+        $sec = &pad($sec);
+        if ($req{$req}{'elapsed'} > 0) {
+            print $req ."\t$year-$month-$day|$hour:$min:$sec\t" .$req{$req}{'elapsed'} . "\n";
+          }
 }
 
