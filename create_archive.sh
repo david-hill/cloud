c5a84acefdcbd7a3c20704eb6ddad27ff1441edf
David Hill <dhill@redhat.com>
2024-01-02 10:11:21 -0500

    Fixed create_archive.sh

--- create_archive.sh
+++ create_archive.sh
@@ -4,10 +4,10 @@ tmpfolder=$(mktemp -d)
 
 url=$1
 #url=http://download-node-02.eng.bos.redhat.com/brewroot/packages/dbus/1.10.22/1.el7/x86_64/
-url=http://download-node-02.eng.bos.redhat.com/brewroot/packages/openstack-neutron-lbaas/9.2.2/7.el7/noarch/
+url=http://download-node-02.eng.bos.redhat.com/brewroot/packages/openstack-neutron/15.2.1/1.20230223123406.el8ost/noarch/
 curl -s $url > $tmpfile
 
-for p in $(cat $tmpfile | grep rpm | awk -F\" '{ print $6 }' | grep -v "\.src"); do
+for p in $(cat $tmpfile | grep rpm | awk -F\" '{ print $8 }' | grep -v "\.src"); do
   wget $url/$p -O $tmpfolder/$p
 done
 
